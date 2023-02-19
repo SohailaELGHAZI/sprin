fadff3627691de9d24c5afa94c78ded39149f7df
sohaila <sohailaelghazi@gmail.com>
2023-02-20 00:20:01 +0100

    initial commit

---
+++ src/main/java/univ/fac/master/ThymApplication.java
@@ -0,0 +1,13 @@
+package univ.fac.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymApplication.class, args);
+	}
+
+}
